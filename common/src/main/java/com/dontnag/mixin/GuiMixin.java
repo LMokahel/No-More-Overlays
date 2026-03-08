36f71f07d2ccf4ee2cfa8b7f6028a15395699700
DontNeg <lukianmokahel123@gmail.com>
2026-03-08 00:41:41 -0800

    Initial 1.21.2-1.21.3 Release. No 1.21.2 Forge, as it does not exist.

--- common/src/main/java/com/dontnag/mixin/GuiMixin.java
+++ common/src/main/java/com/dontnag/mixin/GuiMixin.java
@@ -4,10 +4,9 @@ import com.dontnag.NoMoreOverlays;
 
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,15 +14,24 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Optional;
+
 @Mixin(Gui.class)
 public class GuiMixin {
 
-    @Redirect(method = "renderCameraOverlays", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"))
-    private boolean pumpkinOverlay(ItemStack stack, Item item){
+    @Redirect(method = "renderCameraOverlays", at = @At(value = "INVOKE", target = "Ljava/util/Optional;isPresent()Z"))
+    private boolean pumpkinOverlay(Optional<ResourceLocation> instance){
         if(NoMoreOverlays.config.pumpkin){
             return false;
         }
-        return stack.is(item);
+        return instance.isPresent();
+    }
+
+    @Inject(method = "renderConfusionOverlay", at = @At("HEAD"), cancellable = true)
+    private void nauseaOverlay(GuiGraphics guiGraphics, float f, CallbackInfo ci){
+        if(NoMoreOverlays.config.nausea){
+            ci.cancel();
+        }
     }
 
     @Redirect(method = "renderCameraOverlays", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;getTicksFrozen()I"))
--- forge/src/main/java/com/dontnag/forge/NoMoreOverlaysForge.java
+++ forge/src/main/java/com/dontnag/forge/NoMoreOverlaysForge.java
@@ -4,8 +4,8 @@ import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 
 import net.minecraftforge.client.ConfigScreenHandler;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod(NoMoreOverlaysForge.MOD_ID)
@@ -14,11 +14,11 @@ public class NoMoreOverlaysForge {
     public static final String MOD_ID = "nomoreoverlays";
     public static NoMoreOverlaysConfig config;
 
-    public NoMoreOverlaysForge() {
+    public NoMoreOverlaysForge(FMLJavaModLoadingContext context) {
         AutoConfig.register(NoMoreOverlaysConfig.class, GsonConfigSerializer::new);
         config = AutoConfig.getConfigHolder(NoMoreOverlaysConfig.class).getConfig();
         if(FMLEnvironment.dist.isClient()){
-            ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, NoMoreOverlaysForge::getConfigScreen);
+            context.registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, NoMoreOverlaysForge::getConfigScreen);
         }
     }
 
